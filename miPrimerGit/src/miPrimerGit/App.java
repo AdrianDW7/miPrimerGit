0093174e776e435aad3e174ecaf28c80ec5bace5
ALUMNO2425 <ALUMNO2425@DESKTOP-CDGKK4P>
2026-03-23 10:44:07 +0100

    Cambiando el mensaje de salida. "Hola Mundo Git !!!!!!"

--- miPrimerGit/src/miPrimerGit/App.java
+++ miPrimerGit/src/miPrimerGit/App.java
@@ -13,7 +13,8 @@ public class App {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo !!!!!");
+		// System.out.println("Hola mundo !!!!!");
+		System.out.println("Hola Mundo Git !!!!!");
 	}
 
 }
